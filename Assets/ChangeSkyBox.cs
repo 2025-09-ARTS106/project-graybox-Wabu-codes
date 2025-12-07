0bf68414d039376f36480786fc068bc32c83a3f5
Sunny <the.sunny.cheng@gmail.com>
2025-12-07 17:49:56 -0500

    DayNightCycle!
    - successful day night cycle w/ inputs changed within unity
    - main camera was messing with game camera
    - probuilder texture variants (can edit textures with microsoft paint), can try importing square textures with transparent pngs (import asset) for small details

--- Assets/ChangeSkyBox.cs
+++ Assets/ChangeSkyBox.cs
@@ -1,32 +1,69 @@
+using System.Collections;
 using UnityEngine;
 
 public class SkyBoxChange : MonoBehaviour
 {
-//create editors in inspector menu?
+    //create editors in inspector menu?
     public Material Day;
     public Material Sunset;
     public Material Night;
+    public bool dayNightIsRunning;
+    public float DayLength = 12f;
+    public float FirstDayLength = 3f;
+    public float SunsetLength = 3f;
+    public float NightLength = 6f;
+
     // Start called before first frame update
 
     void Start()
     {
-        Invoke("ChangeSkyBox3", 3); //sunset
-        Invoke("ChangeSkyBox",6); //turn to night (in seconds)
-        Invoke("ChangeSkyBox2", 12); //turn to day
+        dayNightIsRunning = true;
+        StartCoroutine(ChangeSkyboxCoRoutine());
+        //Invoke("ChangeSkyBox3", 3); //sunset
+        //Invoke("ChangeSkyBox", 6); //turn to night (in seconds)
+        //Invoke("ChangeSkyBox2", 12); //turn to day
     }
 
+
+    IEnumerator ChangeSkyboxCoRoutine()
+    {
+        yield return new WaitForSeconds(FirstDayLength); //day
+        ChangeSkyBoxSunset();
+        yield return new WaitForSeconds(3f); //duration of sunset
+        ChangeSkyBoxNight();
+        yield return new WaitForSeconds(6f); //duration of night
+
+        while (dayNightIsRunning)
+        {
+
+            ChangeSkyBoxDay();
+            yield return new WaitForSeconds(DayLength);
+
+            ChangeSkyBoxSunset();
+            yield return new WaitForSeconds(SunsetLength);
+
+            ChangeSkyBoxNight();
+            yield return new WaitForSeconds(NightLength);
+
+        }
+
+        ChangeSkyBoxDay();
+
+    }
+
+
     //Change to night
-    void ChangeSkyBox()
+    void ChangeSkyBoxNight()
     {
         RenderSettings.skybox = Night;
     }
     //Change to day
-    void ChangeSkyBox2()
+    void ChangeSkyBoxDay()
     {
         RenderSettings.skybox = Day;
     }
     //change to sunset
-    void ChangeSkyBox3()
+    void ChangeSkyBoxSunset()
     {
         RenderSettings.skybox = Sunset;
     }
